44bf2b0c83beb86f82ea1726e736183086ffb9e7
Alecs <erick.borges@outlook.es>
2024-03-11 21:15:10 -0600

    fix: Homescreen

--- src/home/HomeScreen.jsx
+++ src/home/HomeScreen.jsx
@@ -10,7 +10,7 @@ import '../styles/homescreen.css';
 
 const mainArticle = [
     {
-        image : 'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg',
+        image: 'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg',
         category: 'Lifestyle',
         title: 'The Greatest Thing in The World is to Know How to Belong to Oneself',
         writer: 'Erick Borges',
@@ -20,7 +20,7 @@ const mainArticle = [
     }
 ];
 
-const featuredtoday=[
+const featuredtoday = [
     {
         category: 'Course',
         title: 'Shoot for the Moon and if You Miss You Will Still Be Among the Stars',
@@ -28,59 +28,59 @@ const featuredtoday=[
     }
 ];
 
-const articles=[
+const articles = [
     {
-        category: 'Lifestyle', 
-        title:'The Two Most Powerfull Warriors Are Patience and Time',
-        writer: 'ErickBorge', 
-        date: 'October 5, 1999', 
-        comentaries: 0, 
-        resume:'Met to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of extra space, including wider seats as standard, no ...',
-        image:'https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        category: 'Lifestyle',
+        title: 'The Two Most Powerfull Warriors Are Patience and Time',
+        writer: 'ErickBorge',
+        date: 'October 5, 1999',
+        comentaries: 0,
+        resume: 'Met to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of extra space, including wider seats as standard, no ...',
+        image: 'https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
     },
     {
-        category: 'Course', 
-        title:'You Will Become as Small as Your Controlling Desire',
-        writer: 'ErickBorge', 
-        date: 'October 5, 1999', 
-        comentaries: 0, 
-        resume:'Met to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of extra space, including wider seats as standard, no ...',
+        category: 'Course',
+        title: 'You Will Become as Small as Your Controlling Desire',
+        writer: 'ErickBorge',
+        date: 'October 5, 1999',
+        comentaries: 0,
+        resume: 'Met to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of extra space, including wider seats as standard, no ...',
         image: 'https://images.pexels.com/photos/267350/pexels-photo-267350.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
     },
     {
-        category: 'TechNews', 
-        title:'The Two Most Powerfull Warriors Are Patience and Time',
-        writer: 'ErickBorge', 
-        date: 'October 5, 1999', 
-        comentaries: 0, 
-        resume:'Met to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of extra space, including wider seats as standard, no ...',
+        category: 'TechNews',
+        title: 'The Two Most Powerfull Warriors Are Patience and Time',
+        writer: 'ErickBorge',
+        date: 'October 5, 1999',
+        comentaries: 0,
+        resume: 'Met to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of extra space, including wider seats as standard, no ...',
         image: 'https://images.pexels.com/photos/123335/pexels-photo-123335.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
     },
     {
-        category: 'Lifestyle', 
-        title:'The Two Most Powerfull Warriors Are Patience and Time',
-        writer: 'ErickBorge', 
-        date: 'October 5, 1999', 
-        comentaries: 0, 
-        resume:'Met to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of extra space, including wider seats as standard, no ...',
-        image:'https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        category: 'Lifestyle',
+        title: 'The Two Most Powerfull Warriors Are Patience and Time',
+        writer: 'ErickBorge',
+        date: 'October 5, 1999',
+        comentaries: 0,
+        resume: 'Met to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of extra space, including wider seats as standard, no ...',
+        image: 'https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
     },
     {
-        category: 'Course', 
-        title:'You Will Become as Small as Your Controlling Desire',
-        writer: 'ErickBorge', 
-        date: 'October 5, 1999', 
-        comentaries: 0, 
-        resume:'Met to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of extra space, including wider seats as standard, no ...',
+        category: 'Course',
+        title: 'You Will Become as Small as Your Controlling Desire',
+        writer: 'ErickBorge',
+        date: 'October 5, 1999',
+        comentaries: 0,
+        resume: 'Met to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of extra space, including wider seats as standard, no ...',
         image: 'https://images.pexels.com/photos/267350/pexels-photo-267350.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
     },
     {
-        category: 'TechNews', 
-        title:'The Two Most Powerfull Warriors Are Patience and Time',
-        writer: 'ErickBorge', 
-        date: 'October 5, 1999', 
-        comentaries: 0, 
-        resume:'Met to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of extra space, including wider seats as standard, no ...',
+        category: 'TechNews',
+        title: 'The Two Most Powerfull Warriors Are Patience and Time',
+        writer: 'ErickBorge',
+        date: 'October 5, 1999',
+        comentaries: 0,
+        resume: 'Met to launch on the manufacturer’s new A330neo aircraft in 2017, it’s offering lots of extra space, including wider seats as standard, no ...',
         image: 'https://images.pexels.com/photos/123335/pexels-photo-123335.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
     },
 ];
@@ -89,37 +89,47 @@ export function HomeScreen() {
     return (
         <article>
             <div className="hs-main">
-                <MainArticle 
+                <MainArticle
                     category={mainArticle[0].category}
                     title={mainArticle[0].title}
                     writer={mainArticle[0].writer}
                     date={mainArticle[0].date}
                     coments={mainArticle[0].coments}
                     resume={mainArticle[0].resume}
+                />
+                {articles.map((article) => (
+                    <ArticlePrev
+                        image={article.image}
+                        category={article.category}
+                        title={article.title}
+                        writer={article.writer}
+                        date={article.date}
+                        coments={article.comentaries}
+                        resume={article.resume}
                     />
-                {articles.map((article)=>(
-                     <ArticlePrev
-                     image={article.image}
-                     category={article.category}
-                     title={article.title}
-                     writer={article.writer}
-                     date={article.date}
-                     coments={article.comentaries}
-                     resume={article.resume}
-                 />
                 ))}
-                <ButtonSearch/>
+                <ButtonSearch />
             </div>
             <div className='hs-extrainfo'>
-                <AboutMe/>
-                <FeaturedToday
-                    category={featuredtoday[0].category}
-                    title={featuredtoday[0].title}
-                    date={featuredtoday[0].date}
-                />
-                <Categories/>
-                <LatestTweets/>
-                <PopularPost/>
+                <div className="hs-aboutme">
+                    <AboutMe />
+                </div>
+                <div className="hs-featured-today">
+                    <FeaturedToday
+                        category={featuredtoday[0].category}
+                        title={featuredtoday[0].title}
+                        date={featuredtoday[0].date}
+                    />
+                </div>
+                <div className="hs-categories">
+                    <Categories />
+                </div>
+                <div className="hs-latestweets">
+                    <LatestTweets />
+                </div>
+                <div className="hs-popular">
+                    <PopularPost />
+                </div>
             </div>
         </article>
 
