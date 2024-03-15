14619bf2a957a20a436466949cd59a020436f66f
Alecs <erick.borges@outlook.es>
2024-03-14 21:45:39 -0600

    feature: List categories

--- src/home/Categories.jsx
+++ src/home/Categories.jsx
@@ -1,38 +1,53 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import { CategoriesCard } from '../components/CategoriesCard';
 import '../styles/categories.css';
 
-const categories = [
-    {
-        categoryName: 'Lifestyle',
-        articlesCount: 6,
-        image: 'https://images.pexels.com/photos/1092671/pexels-photo-1092671.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    },
-    {
-        categoryName: 'Courses',
-        articlesCount: 7,
-        image: 'https://images.pexels.com/photos/4050315/pexels-photo-4050315.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    },
-    {
-        categoryName: 'TechNews',
-        articlesCount: 3,
-        image: 'https://images.pexels.com/photos/6335/man-coffee-cup-pen.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    },
-];
+// const categories = [
+//     {
+//         categoryName: 'Lifestyle',
+//         articlesCount: 6,
+//         image: 'https://images.pexels.com/photos/1092671/pexels-photo-1092671.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+//     },
+//     {
+//         categoryName: 'Courses',
+//         articlesCount: 7,
+//         image: 'https://images.pexels.com/photos/4050315/pexels-photo-4050315.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+//     },
+//     {
+//         categoryName: 'TechNews',
+//         articlesCount: 3,
+//         image: 'https://images.pexels.com/photos/6335/man-coffee-cup-pen.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+//     },
+// ];
 
 export function Categories() {
+
+    // -------- Extraer las categorias --------
+    const [categories, setCategories] = useState([]);
+
+    const loadCategories = async() => {
+        const response = await fetch('http://localhost:3000/categories');
+        const data = await response.json();
+        setCategories(data);
+    }
+
+    useEffect(() => {
+        loadCategories();
+    },[]);
+
+
     return (
         <div className="cat-container">
             <h4>
                 Categories
             </h4>
             {categories.map((category) => (
-                <Link key={category.categoryName} to={`/category/${category.categoryName}`}>
+                <Link key={category.id_cat} to={`/category/${category.nombre}`}>
                     <CategoriesCard
                         image={category.image}
-                        categoryName={category.categoryName}
-                        articlesCount={category.articlesCount}
+                        categoryName={category.nombre}
+                        articlesCount={category.articles}
                     />
                 </Link>
             ))}
